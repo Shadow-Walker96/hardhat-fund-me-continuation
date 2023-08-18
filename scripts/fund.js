6ebfeaa824217eccf4255cd65eedaf740cbd9d1d
Shadow-Walker96 <86225985+Shadow-Walker96@users.noreply.github.com>
2023-08-18 01:56:34 +0000

    completed

---
+++ scripts/fund.js
@@ -0,0 +1,25 @@
+// 12:17:57 ---> Running script on a local node
+
+const { ethers, getNamedAccounts } = require("hardhat")
+
+async function main() {
+  const { deployer } = await getNamedAccounts()
+  const fundMe = await ethers.getContract("FundMe", deployer)
+  console.log(`Got contract FundMe at ${fundMe.address}`)
+  console.log("Funding contract...")
+  const transactionResponse = await fundMe.fund({
+    value: ethers.utils.parseEther("0.1"),
+  })
+  await transactionResponse.wait()
+  console.log("Funded!")
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
+
+//   yarn hardhat node ----> to see all our transaction 
+// yarn hardhat run scripts/fund.js --network localhost
---
+++ scripts/withdraw.js
@@ -0,0 +1,23 @@
+// 12:17:57 ---> Running script on a local node
+
+const { ethers, getNamedAccounts } = require("hardhat")
+
+async function main() {
+  const { deployer } = await getNamedAccounts()
+  const fundMe = await ethers.getContract("FundMe", deployer)
+  console.log(`Got contract FundMe at ${fundMe.address}`)
+  console.log("Withdrawing from contract...")
+  const transactionResponse = await fundMe.withdraw()
+  await transactionResponse.wait()
+  console.log("Got it back!")
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
+
+//   yarn hardhat node ----> to see all our transaction 
+// yarn hardhat run scripts/withdraw.js --network localhost
